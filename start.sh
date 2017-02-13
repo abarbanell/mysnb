bd010713adf6d9bfac803c352e7f2d9d4b338893
Tobias Abarbanell <tobias@abarbanell.de>
2017-02-13 21:31:04 +0100

    added TweetFeed notebook

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# docker pull andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.1-hadoop-2.7.2-with-hive-with-parquet
-# docker run -p 9000:9000 -p 4040-4045:4040-4045 -v `pwd`/data:/opt/docker/data  -v `pwd`/notebooks:/opt/docker/notebooks/mysnb andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.1-hadoop-2.7.2-with-hive-with-parquet
+. .conf.rc
 
-
-# upgraded versions, try without hive and parquet for now, until we have use for it...
- docker pull andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.0.1-hadoop-2.7.2
- docker run -p 9001:9001 -p 4040-4045:4040-4045 \
+# export IMAGE=andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.1-hadoop-2.7.2-with-hive-with-parquet
+# export IMAGE=andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.0.1-hadoop-2.7.2
+export IMAGE=andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.1.0-hadoop-2.7.3-with-hive
+docker pull $IMAGE
+docker run -p 9001:9001 -p 4040-4045:4040-4045 \
 	-v `pwd`/data:/opt/docker/data  \
 	-v `pwd`/notebooks:/opt/docker/notebooks/mysnb \
-	andypetrella/spark-notebook:0.7.0-scala-2.11.8-spark-2.0.1-hadoop-2.7.2
+	-e TWITTER_CONSUMER_KEY=$TWITTER_CONSUMER_KEY \
+	-e TWITTER_CONSUMER_SECRET=$TWITTER_CONSUMER_SECRET \
+	-e TWITTER_ACCESS_TOKEN=$TWITTER_ACCESS_TOKEN \
+	-e TWITTER_ACCESS_TOKEN_SECRET=$TWITTER_ACCESS_TOKEN_SECRET \
+	$IMAGE
